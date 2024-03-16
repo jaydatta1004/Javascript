fe3efaff235b63a63536eebbc72cf0ab70c66d85
jaydatta1004 <120085743+jaydatta1004@users.noreply.github.com>
2024-03-16 12:32:02 +0000

    01_basic

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -1,8 +1,8 @@
 const accoundnumber = 1211323; // Not change in Value
-let accoundname = 'Yogesh'
-var accoundpassword = '12345'
+let accoundname = 'Yogesh';
+var accoundpassword = '12345';
 
-
-accoundname = 'Nitin'
-accoundpassword='6789'
+accoundnumber = 45467;
+accoundname = 'Nitin';
+accoundpassword ='6789';
 console.table(accoundname,accoundnumber,accoundpassword);
